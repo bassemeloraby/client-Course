a5b88401fddbf7c6e5173b123b3704a9edfb6016
bassem <basem12683@gmail.com>
2025-02-20 08:42:27 +0300

    useRouteError

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,8 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <HomeLayout />,
+    errorElement: <Error />,
+
     children: [
       {
         index: true,
@@ -37,10 +39,12 @@ const router = createBrowserRouter([
   {
     path: "/login",
     element: <Login />,
+    errorElement: <Error />,
   },
   {
     path: "/register",
     element: <Register />,
+    errorElement: <Error />,
   },
 ]);
 
