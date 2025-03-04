ceade6df56eb1add718ad536a705e9bd80569146
bassem <basem12683@gmail.com>
2025-03-04 05:12:40 +0300

    Themes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,31 @@
 import { NavLink } from "react-router-dom";
 import NavLinks from "./NavLinks";
+import { BsMoonFill, BsSunFill } from "react-icons/bs";
+import { useEffect, useState } from "react";
 
+const themes = {
+  winter: "winter",
+  dracula: "dracula",
+};
+
+ const getThemeFromLocalStorage = () => {
+   return localStorage.getItem("theme") || themes.winter;
+ };
 const Navbar = () => {
+  const [theme, setTheme] = useState(getThemeFromLocalStorage());
+
+  const handleTheme = () => {
+    const { winter, dracula } = themes;
+    const newTheme = theme === winter ? dracula : winter;
+
+    setTheme(newTheme);
+  };
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
   return (
     <nav className="bg-base-200">
       <div className="navbar align-element ">
@@ -19,7 +43,16 @@ const Navbar = () => {
             <NavLinks />
           </ul>
         </div>
-        <div className="navbar-end">end</div>
+        <div className="navbar-end">
+          {/* THEME SETUP */}
+          <label className="swap swap-rotate">
+            <input type="checkbox" onChange={handleTheme} />
+            {/* sun icon*/}
+            <BsSunFill className="swap-on h-4 w-4" />
+            {/* moon icon*/}
+            <BsMoonFill className="swap-off h-4 w-4" />
+          </label>
+        </div>
       </div>
     </nav>
   );
