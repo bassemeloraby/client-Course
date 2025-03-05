7b5fea63b5e373ae579c9f223e847150dcc04308
bassem <basem12683@gmail.com>
2025-03-05 14:27:34 +0300

    Themes global

--- src/app/features/auth/authSlice.js
+++ src/app/features/auth/authSlice.js
@@ -1,6 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const themes = {
+  winter: "winter",
+  dracula: "dracula",
+};
+
+const getThemeFromLocalStorage = () => {
+  const theme = localStorage.getItem("theme") || themes.winter;
+  document.documentElement.setAttribute("data-theme", theme);
+  return theme;
+};
+
 const initialState = {
+  theme: getThemeFromLocalStorage(),
   user: null,
   isError: false,
   isSuccess: false,
@@ -8,14 +20,20 @@ const initialState = {
   message: "",
 };
 
-
-
 export const authSlice = createSlice({
   name: "auth",
   initialState,
-  reducers: {},
+  reducers: {
+    toggleTheme: (state) => {
+      const { dracula, winter } = themes;
+      state.theme = state.theme === dracula ? winter : dracula;
+      document.documentElement.setAttribute("data-theme", state.theme);
+      localStorage.setItem("theme", state.theme);
+    },
+  },
 });
 
 
+export const { toggleTheme } = authSlice.actions;
 
 export default authSlice.reducer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,18 @@
 import { NavLink } from "react-router-dom";
 import NavLinks from "./NavLinks";
 import { BsMoonFill, BsSunFill } from "react-icons/bs";
-import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { toggleTheme } from "../app/features/auth/authSlice";
 
-const themes = {
-  winter: "winter",
-  dracula: "dracula",
-};
 
- const getThemeFromLocalStorage = () => {
-   return localStorage.getItem("theme") || themes.winter;
- };
+ 
 const Navbar = () => {
-  const [theme, setTheme] = useState(getThemeFromLocalStorage());
+  const dispatch = useDispatch();
 
   const handleTheme = () => {
-    const { winter, dracula } = themes;
-    const newTheme = theme === winter ? dracula : winter;
-
-    setTheme(newTheme);
+       dispatch(toggleTheme());
+ 
   };
-
-  useEffect(() => {
-    document.documentElement.setAttribute("data-theme", theme);
-    localStorage.setItem("theme", theme);
-  }, [theme]);
 
   return (
     <nav className="bg-base-200">
